e9d3d7bbc50d673c80e35879a5216a5012f14020
Deepak Pathak <shuunya@Deepaks-MacBook-Air.local>
2024-04-21 21:19:07 +0530

    Created Order.dispatched topic for order dispatched producer for outbound to consume at console

--- src/test/java/com/kafka/example/dispatch/handler/OrderCreatedHandlerTest.java
+++ src/test/java/com/kafka/example/dispatch/handler/OrderCreatedHandlerTest.java
@@ -14,18 +14,27 @@ import static org.mockito.Mockito.*;
 public class OrderCreatedHandlerTest {
 
     private OrderCreatedHandler handler;
-    private DispatchService serviceMock;
+    private DispatchService dispatchServiceMock;
 
     @BeforeEach
     void setup() {
-        serviceMock = mock(DispatchService.class);
-        handler = new OrderCreatedHandler(serviceMock);
+        dispatchServiceMock = mock(DispatchService.class);
+        handler = new OrderCreatedHandler(dispatchServiceMock);
     }
 
     @Test
-    void listen() {
+    void listen_Success() throws Exception{
         OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
         handler.listen(testEvent);
-        verify(serviceMock, times(1)).process(testEvent);
+        verify(dispatchServiceMock, times(1)).process(testEvent);
+    }
+
+    @Test
+    public void listen_ServiceThrowsException() throws Exception{
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+        doThrow(new RuntimeException("Service Failure")).when(dispatchServiceMock).process(testEvent);
+
+        handler.listen(testEvent);
+        verify(dispatchServiceMock, times(1)).process(testEvent);
     }
 }
